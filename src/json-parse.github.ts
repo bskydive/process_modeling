1c2dcdfda6ba39e098ff6a3f5806e6cae56016fa
Valeriy Stepanov <stepanovv.ru@yandex.ru>
2023-06-02 17:57:06 +0300

    добавил типы данных и заглушки для скриптов

--- src/json-parse.github.ts
+++ src/json-parse.github.ts
@@ -1,15 +1,63 @@
-const { writeFileSync } = require('fs');
+import * as issues from "./vscode.issues-closed.response.json";
+import { IGithubIssues } from "./vscode.issues-closed.response";
+import { IGithubPulls } from "./vscode.pulls-closed.response";
 
-const path = './config.json';
-const config = { ip: '192.0.2.1', port: 3000 };
+const { fs } = require("fs");
 
-try {
-  writeFileSync(path, JSON.stringify(config, null, 2), 'utf8');
-  console.log('Data successfully saved to disk');
-} catch (error) {
-  console.log('An error has occurred ', error);
+const path = "./log";
+const data = {};
+
+const issuesMock: IGithubIssues[] = issues;
+const pullsMock: IGithubPulls[] = issues;
+
+function listFiles(path: string): string[] {
+	let result: string[] = [];
+
+	try {
+		result = fs.readdirSync(path) || [];
+		console.log("read", path, result.length);
+	} catch (error) {
+		console.log("FS read error:", error);
+	}
+
+	return result;
 }
 
-export function main(){
-	console.log('start');
+function readData(path: string, fileName: string): string {
+	let result: string = "";
+
+	try {
+		result = fs.readFileSync(path) || "";
+		console.log("read", path, fileName);
+	} catch (error) {
+		console.log("FS read error:", error);
+	}
+
+	return result;
+}
+
+function writeData(path: string, fileName: string, data: any) {
+	try {
+		fs.writeFileSync(path, JSON.stringify(data, null, 4), "utf8");
+		console.log("write", path, fileName);
+	} catch (error) {
+		console.log("FS write error:", error);
+	}
+}
+
+function parseIssuesFile(data: string) {
+	/*
+	 * json поля issues
+	 * number
+	 * id
+	 * user.id
+	 * assignee.id
+	 * milestone.id
+	 * created_at
+	 * closed_at
+	 */
+}
+
+export function main() {
+	console.log("start", issuesMock.length, pullsMock.length);
 }
