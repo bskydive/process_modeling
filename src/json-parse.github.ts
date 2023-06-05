aa4aa2b854dddd694cc11fdeb79b9865b73e250d
Valeriy Stepanov <stepanovv.ru@yandex.ru>
2023-06-05 18:44:38 +0300

    сделал предварительный парсинг и валидацию

--- src/json-parse.github.ts
+++ src/json-parse.github.ts
@@ -1,21 +1,24 @@
 import * as issues from "./vscode.issues-closed.response.json";
-import { IGithubIssues } from "./vscode.issues-closed.response";
-import { IGithubPulls } from "./vscode.pulls-closed.response";
+import {
+	IGithubIssue,
+	IGithubIssueParsed,
+} from "./vscode.issues-closed.response";
+import { IGithubPull, IGithubPullParsed } from "./vscode.pulls-closed.response";
 
-const { fs } = require("fs");
+import * as fs from "node:fs";
 
 const path = "./log";
 const data = {};
 
-const issuesMock: IGithubIssues[] = issues;
-const pullsMock: IGithubPulls[] = issues;
+const issuesMock: IGithubIssue[] = issues;
+const pullsMock: IGithubPull[] = issues;
 
 function listFiles(path: string): string[] {
 	let result: string[] = [];
 
 	try {
-		result = fs.readdirSync(path) || [];
-		console.log("read", path, result.length);
+		result = fs.readdirSync(path, "utf-8") || [];
+		// console.log("list", path, result.length);
 	} catch (error) {
 		console.log("FS read error:", error);
 	}
@@ -27,8 +30,8 @@ function readData(path: string, fileName: string): string {
 	let result: string = "";
 
 	try {
-		result = fs.readFileSync(path) || "";
-		console.log("read", path, fileName);
+		// console.log("read", path, fileName);
+		result = fs.readFileSync(path + fileName, "utf-8") || "";
 	} catch (error) {
 		console.log("FS read error:", error);
 	}
@@ -45,19 +48,96 @@ function writeData(path: string, fileName: string, data: any) {
 	}
 }
 
-function parseIssuesFile(data: string) {
-	/*
-	 * json поля issues
-	 * number
-	 * id
-	 * user.id
-	 * assignee.id
-	 * milestone.id
-	 * created_at
-	 * closed_at
-	 */
+function parseIssuesFile(rawFile: string): IGithubIssueParsed[] {
+	const issueList: IGithubIssue[] = JSON.parse(rawFile);
+	const result: IGithubIssueParsed[] = [];
+
+	issueList.forEach((issue:IGithubIssue)=>result.push({
+		url: issue.html_url,
+		id: issue.id,
+		number: issue.number,
+		title: issue.title,
+		user_id: issue?.user?.id || null,
+		state: issue.state,
+		assignee_id: issue?.assignee?.id || null,
+		comments_length: issue.comments,
+		created: issue.created_at,
+		updated: issue.updated_at,
+		closed: issue.closed_at,
+		body: issue.body,
+	}));
+
+	return result;
+}
+
+function parsePullsFile(rawFile: string): IGithubPullParsed[] {
+	const issueList: IGithubPull[] = JSON.parse(rawFile);
+	const result: IGithubPullParsed[] = [];
+
+	issueList.forEach((pull:IGithubPull)=>result.push({
+		url: pull.html_url,
+		id: pull.id,
+		number: pull.number,
+		title: pull.title,
+		user_id: pull?.user?.id || null,
+		state: pull.state,
+		assignee_id: pull?.assignee?.id || null,
+		created: pull.created_at,
+		updated: pull.updated_at,
+		closed: pull.closed_at,
+		merged: pull.merged_at,
+		body: pull.body,
+		merge_commit_sha: pull.merge_commit_sha,
+	}));
+
+	return result;
+}
+
+function parseIssues(): IGithubIssueParsed[]{
+	const pathIssues: string = "./log/issues.responses/";
+	const issuesFileNameList: string[] = listFiles(pathIssues);
+	let parsedIssues: IGithubIssueParsed[] = [];
+
+	issuesFileNameList.forEach((fileName: string) => {
+		const rawFile = readData(pathIssues, fileName);
+		// console.log("parse issue", path, fileName);
+		parsedIssues.push(...parseIssuesFile(rawFile));
+	});
+
+	console.log(
+		"issues parsed",
+		issuesFileNameList[0],
+		issuesFileNameList.length,
+		parsedIssues[0],
+		parsedIssues.length,
+	);
+
+	return parsedIssues;
+}
+
+function parsePulls(): IGithubPullParsed[]{
+	const pathPulls: string = "./log/pulls.responses/";
+	const pullsFileNameList: string[] = listFiles(pathPulls);
+	let parsedPulls: IGithubPullParsed[] = [];
+
+	pullsFileNameList.forEach((fileName: string) => {
+		const rawFile = readData(pathPulls, fileName);
+		// console.log("parse pull", path, fileName);
+		parsedPulls.push(...parsePullsFile(rawFile));
+	});
+
+	console.log(
+		"pulls parsed",
+		pullsFileNameList[0],
+		pullsFileNameList.length,
+		parsedPulls[0],
+		parsedPulls.length
+	);
+
+	return parsedPulls;
 }
 
 export function main() {
-	console.log("start", issuesMock.length, pullsMock.length);
+	const parsedIssues: IGithubIssueParsed[] = parseIssues();
+	const parsedPulls: IGithubPullParsed[] = parsePulls();
 }
--- src/vscode.issues-closed.response.ts
+++ src/vscode.issues-closed.response.ts
@@ -1,94 +1,108 @@
-export interface IGithubIssues {
+export interface IGithubIssueParsed {
+	url: string; // "html_url": "https://github.com/microsoft/vscode/issues/184041",
+	id: number; // "id": 1735940057,
+	number: number; // "number": 184041,
+	title: string; // "title": "Setting `snippetsPreventQuickSuggestions` is not considered",
+	user_id: number | null; // "id": 2931520,
+	state: string; // "state": "closed",
+	assignee_id: number | null; // "id": 1794099,
+	comments_length: number; // "comments": 0,
+	created: string; // "created_at": "2023-06-01T10:01:54Z",
+	updated: string; // "updated_at": "2023-06-01T21:04:32Z",
+	closed: string; // "closed_at": "2023-06-01T10:21:27Z",
+	body: string; // "body": "> turn snippetsPreventQuickSuggestions to true and ensure typing in a snippet placeholder doesn't
+}
 
-		url: string;
-		repository_url: string;
-		labels_url: string;
-		comments_url: string;
-		events_url: string;
-		html_url: string;
-		id: number;
-		node_id: string;
-		number: number;
-		title: string;
-		user: User;
-		labels: Label[];
-		state: string;
-		locked: boolean;
-		assignee?: User | null;
-		assignees: User[];
-		milestone: Milestone;
-		comments: number;
-		created_at: string;
-		updated_at: string;
-		closed_at: string;
-		author_association: string;
-		active_lock_reason?: any;
-		body: string;
-		reactions: Reactions;
-		timeline_url: string;
-		performed_via_github_app?: any;
-		state_reason: string;
-	  }
-	  
-	  interface Reactions {
-		url: string;
-		total_count: number;
-		'+1': number;
-		'-1': number;
-		laugh: number;
-		hooray: number;
-		confused: number;
-		heart: number;
-		rocket: number;
-		eyes: number;
-	  }
-	  
-	  interface Milestone {
-		url: string;
-		html_url: string;
-		labels_url: string;
-		id: number;
-		node_id: string;
-		number: number;
-		title: string;
-		description: string;
-		creator?: any;
-		open_issues: number;
-		closed_issues: number;
-		state: string;
-		created_at: string;
-		updated_at: string;
-		due_on: string;
-		closed_at?: any;
-	  }
-	  
-	  interface Label {
-		id: number;
-		node_id: string;
-		url: string;
-		name: string;
-		color: string;
-		default: boolean;
-		description: string;
-	  }
-	  
-	  interface User {
-		login: string;
-		id: number;
-		node_id: string;
-		avatar_url: string;
-		gravatar_id: string;
-		url: string;
-		html_url: string;
-		followers_url: string;
-		following_url: string;
-		gists_url: string;
-		starred_url: string;
-		subscriptions_url: string;
-		organizations_url: string;
-		repos_url: string;
-		events_url: string;
-		received_events_url: string;
-		type: string;
-		site_admin: boolean;
-	  }
+export interface IGithubIssue {
+	url: string;
+	repository_url: string;
+	labels_url: string;
+	comments_url: string;
+	events_url: string;
+	html_url: string;
+	id: number;
+	node_id: string;
+	number: number;
+	title: string;
+	user: User;
+	labels: Label[];
+	state: string;
+	locked: boolean;
+	assignee?: User | null;
+	assignees: User[];
+	milestone: Milestone;
+	comments: number;
+	created_at: string;
+	updated_at: string;
+	closed_at: string;
+	author_association: string;
+	active_lock_reason?: any;
+	body: string;
+	reactions: Reactions;
+	timeline_url: string;
+	performed_via_github_app?: any;
+	state_reason: string;
+}
+
+interface Reactions {
+	url: string;
+	total_count: number;
+	"+1": number;
+	"-1": number;
+	laugh: number;
+	hooray: number;
+	confused: number;
+	heart: number;
+	rocket: number;
+	eyes: number;
+}
+
+interface Milestone {
+	url: string;
+	html_url: string;
+	labels_url: string;
+	id: number;
+	node_id: string;
+	number: number;
+	title: string;
+	description: string;
+	creator?: any;
+	open_issues: number;
+	closed_issues: number;
+	state: string;
+	created_at: string;
+	updated_at: string;
+	due_on: string;
+	closed_at?: any;
+}
+
+interface Label {
+	id: number;
+	node_id: string;
+	url: string;
+	name: string;
+	color: string;
+	default: boolean;
+	description: string;
+}
+
+interface User {
+	login: string;
+	id: number;
+	node_id: string;
+	avatar_url: string;
+	gravatar_id: string;
+	url: string;
+	html_url: string;
+	followers_url: string;
+	following_url: string;
+	gists_url: string;
+	starred_url: string;
+	subscriptions_url: string;
+	organizations_url: string;
+	repos_url: string;
+	events_url: string;
+	received_events_url: string;
+	type: string;
+	site_admin: boolean;
+}
